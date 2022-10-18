2348827d2097885f009c598fb324c05188b6120a
PALR-1216 <palr12162000@gmail.com>
2022-10-18 19:52:51 -0400

    dfd

--- App.js
+++ App.js
@@ -1,20 +1,85 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, TextInput, View, Button , Alert} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+export default class App extends React.Component{
+  constructor() {
+    super()
+    this.state = {
+      username:'',
+      password:''
+    }
+  }
+
+  onLogin () {
+    const{username,password} = this.state
+
+    if(!username) {
+      Alert.alert("please enter username")
+    }
+
+    // Alert.alert('Credentials', `${username} + ${password}`);
+
+    else if(!password) {
+      Alert.alert("Please enter password")
+    }
+
+    else {
+
+      Alert.alert(`username ${username} password ${password}`)
+
+    }
+  }
+
+  
+  render() {
+    return(
+      <View style={styles.container}>
+        <StatusBar hidden={true}/>
+        <TextInput
+        value={this.state.username}
+        onChangeText={(username) => this.setState({username})}
+        placeholder={'Username'}
+        style={styles.input}
+        
+        />
+
+      <TextInput
+        value={this.state.password}
+        onChangeText={(password) => this.setState({password})}
+        placeholder={'Password'}
+        style={styles.input}
+        
+        />
+
+          <Button
+          title={'Login'}
+          style={styles.input}
+          onPress={this.onLogin.bind(this)}
+            
+
+        />
+      </View>
+    )
+
+  }
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+  },
+  input: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 10,
   },
 });
