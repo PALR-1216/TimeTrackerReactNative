216da5adc7d8efa36415916e816e4ead14d67a11
PALR-1216 <palr12162000@gmail.com>
2022-10-19 02:06:11 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, TextInput, View, Button , Alert} from 'react-native';
+const API_URL = 'https://myworktimetracker.herokuapp.com'
 
 
 export default class App extends React.Component{
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
       username:'',
-      password:''
+      password:'',
+      isLogin:false
     }
   }
 
@@ -16,19 +18,42 @@ export default class App extends React.Component{
     const{username,password} = this.state
 
     if(!username) {
-      Alert.alert("please enter username")
+      Alert.alert("please enter Credentials")
     }
 
     // Alert.alert('Credentials', `${username} + ${password}`);
 
     else if(!password) {
-      Alert.alert("Please enter password")
+      Alert.alert("Please enter Credentials")
     }
 
     else {
+      const payLoad = {
+        userName:username,
+        password:password
+      }
 
-      Alert.alert(`username ${username} password ${password}`)
+      // Alert.alert(`username ${username} password ${password}`)
+      
+      fetch(`${API_URL}/Apilogin`,{
+        method:"POST",
+        headers: {
+          'Content-Type': 'application/json',
+      },
 
+      body:JSON.stringify(payLoad)
+      })
+      .then(async res =>{
+        try{
+          let jsonRes = await res.json()
+          console.log(jsonRes)
+          this.setState({isLogin:true})
+          Alert.alert(`User ${jsonRes[0].userName} Found`)
+
+        }catch(error) {
+          Alert.alert(`User was not found`)
+        }
+      })
     }
   }
 
